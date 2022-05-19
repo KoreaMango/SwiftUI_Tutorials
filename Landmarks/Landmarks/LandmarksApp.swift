4e349e728d1983d566f2bc9806802016384cd663
Mingyu Kang <57595198+KoreaMango@users.noreply.github.com>
2022-05-19 17:23:09 +0900

    SwiftUI Tutorials Init

---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by 강민규 on 2022/05/19.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
